3228347272e4ddf937d241baee57ee04a8b68814
Luan Nguyen <luan.ndd.60cntt@ntu.edu.vn>
2023-04-11 16:51:18 +0700

    no message

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def projects(request):
+    return render(request, 'websites/projects.html')
+
+def pageTwo(request, pk):
+    return render(request,'websites/single-project.html')
