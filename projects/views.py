ee3d13ab7a7bb8bb9ae2308647d86285555e7e4a
Luan Nguyen <luan.ndd.60cntt@ntu.edu.vn>
2023-04-12 17:00:00 +0700

    no message

--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,51 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+projectItem = [
+    {
+        'idProduct': '1',
+        'nameProduct': 'Product 1',
+        'infoProduct': 'Info for Items 1',
+        'pricesProduct': '14.56',
+        'state': True
+    },
+    {
+        'idProduct': '2',
+        'nameProduct': "Product 2",
+        'infoProduct': "Info for Items 2",
+        'pricesProduct': "76.56",
+        'state': False
+    },
+    {
+        'idProduct': '3',
+        'nameProduct': "Product 2",
+        'infoProduct': "Info for Items 3",
+        'pricesProduct': "24.56",
+        'state': True
+    },
+    {
+        'idProduct': '4',
+        'nameProduct': "Product 1",
+        'infoProduct': "Info for Items 4",
+        'pricesProduct': "17.56",
+        'state': True
+    }
+]
+
+
 def projects(request):
-    return render(request, 'websites/projects.html')
+    name = 'Duy Luan'
+    age = 23
+
+    context = {'projects': projectItem}
+    return render(request, 'websites/projects.html', context)
+
 
 def pageTwo(request, pk):
-    return render(request,'websites/single-project.html')
+    projectObject = None
+
+    for i in projectItem:
+        if i['idProduct'] == str(pk):
+            projectObject = i
+
+    return render(request, 'websites/single-project.html', {'project': projectObject})
