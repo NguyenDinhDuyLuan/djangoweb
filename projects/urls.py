e7969cdbdf0a8a0f83e9dafafec3bb63f8c73001
Luan Nguyen <luan.ndd.60cntt@ntu.edu.vn>
2023-04-17 18:03:51 +0700

    no message

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.projects, name="projects"),
     path('pageTwo/<str:pk>/', views.pageTwo, name="project"),
+    path('create-project/', views.createProject, name="create-project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,39 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Project
+from .form import ProjectForm
 
-projectItem = [
-    {
-        'idProduct': '1',
-        'nameProduct': 'Product 1',
-        'infoProduct': 'Info for Items 1',
-        'pricesProduct': '14.56',
-        'state': True
-    },
-    {
-        'idProduct': '2',
-        'nameProduct': "Product 2",
-        'infoProduct': "Info for Items 2",
-        'pricesProduct': "76.56",
-        'state': False
-    },
-    {
-        'idProduct': '3',
-        'nameProduct': "Product 3",
-        'infoProduct': "Info for Items 3",
-        'pricesProduct': "24.56",
-        'state': True
-    },
-    {
-        'idProduct': '4',
-        'nameProduct': "Product 4",
-        'infoProduct': "Info for Items 4",
-        'pricesProduct': "17.56",
-        'state': True
-    }
-]
+# projectItem = [
+#     {
+#         'idProduct': '1',
+#         'nameProduct': 'Product 1',
+#         'infoProduct': 'Info for Items 1',
+#         'pricesProduct': '14.56',
+#         'state': True
+#     },
+#     {
+#         'idProduct': '2',
+#         'nameProduct': "Product 2",
+#         'infoProduct': "Info for Items 2",
+#         'pricesProduct': "76.56",
+#         'state': False
+#     },
+#     {
+#         'idProduct': '3',
+#         'nameProduct': "Product 3",
+#         'infoProduct': "Info for Items 3",
+#         'pricesProduct': "24.56",
+#         'state': True
+#     },
+#     {
+#         'idProduct': '4',
+#         'nameProduct': "Product 4",
+#         'infoProduct': "Info for Items 4",
+#         'pricesProduct': "17.56",
+#         'state': True
+#     }
+# ]
 
 
 def projects(request):
@@ -48,3 +49,16 @@ def pageTwo(request, pk):
     reviews = projectObj.review_set.all()
     context = {'project': projectObj, 'tags': tags, 'reviews': reviews}
     return render(request, 'websites/single-project.html', context)
+
+
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+        
+    context = {'form': form}
+    return render(request, 'websites/project-form.html', context)
